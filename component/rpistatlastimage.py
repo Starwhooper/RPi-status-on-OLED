a5f6472278dc5121aff93eb5738c5fb86a6a0a56
Starwhooper <thiemo@schuff.eu>
2021-12-27 15:48:09 +0100

    Update rpistatlastimage.py

--- component/rpistatlastimage.py
+++ component/rpistatlastimage.py
@@ -4,17 +4,20 @@ def output(cf,lcd_width):
  import socket
  import glob
  import os
+
+ global lastimagemarqueepos
+ global lastimagemarqueewait
  
  hostname = str(socket.gethostname()).upper()
  
  image = Image.new("RGB", (lcd_width, 10), cf["backgroundcolor"])
  draw = ImageDraw.Draw(image)
  
-# try: marqueepos
-# except: marqueepos = 0
-# try: marqueewait
-# except: marqueewait = 0
-# if 'latest_file' not in locals():
+ try: lastimagemarqueepos
+ except: lastimagemarqueepos = 0
+ try: lastimagemarqueewait
+ except: lastimagemarqueewait = 0
+
  checkforlatestfile = str(cf["checkforlatestfile"]).replace("%HOSTNAME%", str(hostname).lower())
  list_of_files = glob.glob(checkforlatestfile)
  if len(list_of_files) == 0:
@@ -24,17 +27,15 @@ def output(cf,lcd_width):
   latest_file = max(list_of_files, key=os.path.getctime)
   latest_file_name = os.path.basename(latest_file)
   latest_file_name_text = 'IMG: ' + latest_file_name
-#  marqueewidth, marqueewidthheight = draw.textsize(latest_file_name_text)
-#  if marqueepos <= LCD_1in44.LCD_WIDTH - marqueewidth:
-#   marqueewait = marqueewait + 1
-#  else: marqueepos = marqueepos - 2
-#  if marqueewait > cf["scrollingtextwait"] / cf["imagerefresh"]:
-#   marqueepos = 0
-#   marqueewait = 0
-  draw.text((0 ,0), latest_file_name_text, fill = cf["fontcolor"])
-
+  marqueewidth, marqueewidthheight = draw.textsize(latest_file_name_text)
+  if lastimagemarqueepos <= lcd_width - marqueewidth:
+   lastimagemarqueewait = lastimagemarqueewait + 1
+  else: lastimagemarqueepos = lastimagemarqueepos - 2
+  if lastimagemarqueewait > cf["scrollingtextwait"] / cf["imagerefresh"]:
+   lastimagemarqueepos = 0
+   lastimagemarqueewait = 0
+  draw.text((lastimagemarqueepos ,0), latest_file_name_text, fill = cf["fontcolor"])
  
  bannerwidth, bannerheight = image.size
  
  return (image,bannerheight)
- 
