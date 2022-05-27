36fa267c4ad6eaa9d9ef365676d08c1859ccf601
Starwhooper <thiemo@schuff.eu>
2022-05-27 20:38:37 +0200

    show version of script

---
+++ component/rpistatversion.py
@@ -0,0 +1,19 @@
+def output(cf,lcd_width):
+ from PIL import Image
+ from PIL import ImageDraw
+# import netifaces
+# import time
+ import os
+ import re
+ import subprocess
+
+ image = Image.new("RGB", (lcd_width, 13), cf["backgroundcolor"])
+ draw = ImageDraw.Draw(image)
+ 
+ v = re.sub('[^0-9\-]+', '', str(subprocess.check_output('git show -s --format=%cd --date=format:\'%y%m%d-%H%M\'', shell=True)))
+ 
+ draw.text((0,0), "Scpt:" + v, fill = cf["fontcolor"])
+
+ bannerwidth, bannerheight = image.size
+ 
+ return (image,bannerheight)
