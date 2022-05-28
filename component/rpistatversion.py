14ea0ac903ea202a2cfa271935f7e8aa5d5752b2
Starwhooper <thiemo@schuff.eu>
2022-05-28 15:08:17 +0200

    Update rpistatversion.py

--- component/rpistatversion.py
+++ component/rpistatversion.py
@@ -10,7 +10,7 @@ def output(cf,lcd_width):
  image = Image.new("RGB", (lcd_width, 13), cf["backgroundcolor"])
  draw = ImageDraw.Draw(image)
  
- v = re.sub('[^0-9\-]+', '', str(subprocess.check_output('git -C ' + (os.path.split(os.path.abspath(__file__))[0] + ' show -s --format=%cd --date=format:\'%y%m%d-%H%M\'', shell=True)))
+ v = re.sub('[^0-9\-]+', '', str(subprocess.check_output('git -C ' + os.path.split(os.path.abspath(__file__))[0] + ' show -s --format=%cd --date=format:\'%y%m%d-%H%M\'', shell=True)))
  
  draw.text((0,0), "Scpt:" + v, fill = cf["fontcolor"])
 
